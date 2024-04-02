e7d8dc06192eaa4ae13716653210b63b2140d956
Petar Patev <petarpatev@abv.bg>
2024-04-02 10:47:25 +0300

    Define loop engine

---
+++ src/gameEngine.js
@@ -0,0 +1,3 @@
+function gameAction() {
+    window.requestAnimationFrame(gameAction);
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 let elements = initElements();
 let state = initState();
 
-elements.startScreen.addEventListener('click', (e) => {
+elements.startScreen.addEventListener('click', onGameStart);
+
+function onGameStart() {
     elements.startScreen.classList.add('hide');
     elements.gameScreen.classList.remove('hide');
-})
+
+    //start game
+    window.requestAnimationFrame(gameAction);
+}
